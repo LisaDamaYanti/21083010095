169339e3ed17085b305575c1abea329e9dd68225
LisaDamaYanti <113020485+LisaDamaYanti@users.noreply.github.com>
2022-09-17 11:06:39 -0400

    Add files via upload

---
+++ percabangan2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printf "cowo fiksi yang bisa kamu gapai ?\n"
+printf "Taeyong ?\n"
+printf "Jeno ?\n"
+printf "Lucas?\n"
+
+read cowo fiksi
+
+case "$cowo fiksi" in 
+   "Taeyong")
+     echo "Taeyong duplikat anime";;
+   "Jeno") 
+     echo "Jeno kiyowo0ok";;
+   "Lucas")
+     echo "Lucas cuakepppp polll";;
+   *)
+     echo "cowo fiksimu gabisa digapai, gausa halu wkwk";;
+esac
